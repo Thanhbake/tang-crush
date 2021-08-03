f766071a84427a14d39ceaed49482a32c188ab64
Thanhbake <88365712+Thanhbake@users.noreply.github.com>
2021-08-03 13:21:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều nà 😘😘',
+    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
     messDesc: 'Hết dịch anh đưa công chúa đi Măng Đen chơi nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.instagram.com/thanhbake/' //link mess của các bạn. VD: https://m.me/nam.nodemy
