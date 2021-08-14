f400fe60d0439bfd5e132718e6125a8426589927
Thanhbake <88365712+Thanhbake@users.noreply.github.com>
2021-08-14 10:47:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Chị à!',
+    introTitle: 'Bạn à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao Bạn cứ đi nhầm đường
+    Lạc vào tim Bake lẻ loi`,
     btnIntro: 'hihi',
-    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    title: 'Phải chăng Bạn đã yêu chúng mình ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Bạn sẽ ghé Bake để Chill và tận hưởng ly cocktail chứ? ',
+    btnYes: 'Tất nhiên rùi nè <33',
+    btnNo: 'Không ghé đâu, à hi hi :3',
+    question:'Trên thế giới hơn 100 cái rooftop mà Bạn vẫn chọn Bake <3',
+    btnReply: 'Gửi cho chúng mình biết nha <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Hết dịch anh đưa công chúa đi Măng Đen chơi nhé.',
+    mess: 'Bake biết mà 🥰. Yêu bạn nhiều lắm nun 😘😘',
+    messDesc: 'Phải ghé quán Bake của chúng mình đó nhoa',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.instagram.com/thanhbake/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
